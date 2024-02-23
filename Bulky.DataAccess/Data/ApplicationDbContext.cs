91962a5886c60a03bc68e94b3a59c7cd242ba71f
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2024-02-23 11:46:04 +0100

    added migration

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -39,7 +39,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 85,
                     Price100 = 80,
                     CategoryId = 1,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-1/240def0e-ff8e-4b61-bc03-93ce31069e55.jpg"
                 },
                 new Product
                 {
@@ -53,7 +53,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 25,
                     Price100 = 20,
                     CategoryId = 1,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-2/869b10fd-27c9-4219-ab87-8a1fbd82005c.jpg"
                 },
                 new Product
                 {
@@ -67,7 +67,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 40,
                     Price100 = 35,
                     CategoryId = 1,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-3/bb02ab8a-93e0-4051-afd3-55f74dd9c93e.jpg"
                 },
                 new Product
                 {
@@ -81,7 +81,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 60,
                     Price100 = 55,
                     CategoryId = 2,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-4/a782cc8c-6a2f-4c71-8bdb-bd536e0cc32f.jpg"
                 },
                 new Product
                 {
@@ -95,7 +95,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 25,
                     Price100 = 20,
                     CategoryId = 2,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-5/49c0909e-c181-4e47-aedc-63a81774214a.jpg"
                 },
                 new Product
                 {
@@ -109,7 +109,7 @@ namespace BulkyBook.DataAccess.Data
                     Price50 = 22,
                     Price100 = 20,
                     CategoryId = 3,
-                    ImageUrl = ""
+                    ImageUrl = "https://bulky.azurewebsites.net/images/products/product-6/ee6e44e0-3eab-41e1-8f4d-402c927d969a.jpg"
                 }
                 );
         }
---
+++ Bulky.DataAccess/Migrations/20240223104457_addedUrlOfBooks.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedUrlOfBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-1/240def0e-ff8e-4b61-bc03-93ce31069e55.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-2/869b10fd-27c9-4219-ab87-8a1fbd82005c.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-3/bb02ab8a-93e0-4051-afd3-55f74dd9c93e.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-4/a782cc8c-6a2f-4c71-8bdb-bd536e0cc32f.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-5/49c0909e-c181-4e47-aedc-63a81774214a.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "https://bulky.azurewebsites.net/images/products/product-6/ee6e44e0-3eab-41e1-8f4d-402c927d969a.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
